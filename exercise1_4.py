4d4e7597c3b75fc0eab7b7f21ec86270634461ca
soumya1401 <51019674+soumya1401@users.noreply.github.com>
2019-05-30 15:00:44 +0530

    Delete exercise1_4.py

--- exercise1_4.py
+++
@@ -1,8 +0,0 @@
-
-
-numbers = input("Enter numbers whose factorial is needed separating with comma:")
-my_list= numbers.split(",")
-my_list = list(my_list)
-print(my_list)
-my_tuple = tuple(my_list)
-print(my_tuple)
